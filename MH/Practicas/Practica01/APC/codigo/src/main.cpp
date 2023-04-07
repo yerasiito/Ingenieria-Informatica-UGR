f4075e1a887aabcb6f953772eb1ff6e7afc166c3
yerasiito <ylopezramirez02@gmail.com>
2023-04-07 03:00:44 +0200

    Feat: algoritmo por parametro

--- MH/Practicas/Practica01/APC/codigo/src/main.cpp
+++ MH/Practicas/Practica01/APC/codigo/src/main.cpp
@@ -6,7 +6,6 @@
 #include <filesystem>
 #include <chrono>
 using namespace std;
-using namespace std::chrono;
 
 string path = "../Instancias_APC/";
 
@@ -45,13 +44,15 @@ int leerFicheros(string fichero, Dataset &train, Dataset &test, int k){
     return EXIT_SUCCESS;
 }
 
-vector<double> calcularRendimiento(int i, int acierto_train, int acierto_test, Dataset train, Dataset test, vector<double> w, high_resolution_clock::time_point momentoInicio){
+vector<double> calcularRendimiento(int i, int acierto_train, int acierto_test, Dataset train, Dataset test, vector<double> w,
+    chrono::high_resolution_clock::time_point momentoInicio){
+    
     double tasa_clas_train = 100.0*acierto_train/train.numEjemplos();
     double tasa_clas_test = 100.0*acierto_test/test.numEjemplos();
     double tasa_red = calcularTasaRed(w);
     double fitness = 0.8*tasa_clas_test + 0.2*tasa_red;
-    auto momentoFin = high_resolution_clock::now();
-    milliseconds tiempo = duration_cast<std::chrono::milliseconds>(momentoFin - momentoInicio);
+    auto momentoFin = chrono::high_resolution_clock::now();
+    chrono::milliseconds tiempo = chrono::duration_cast<chrono::milliseconds>(momentoFin - momentoInicio);
     double t = double(tiempo.count());
 
     vector<double> rendimiento = {tasa_clas_train, tasa_clas_test, tasa_red, fitness, t};
@@ -60,26 +61,26 @@ vector<double> calcularRendimiento(int i, int acierto_train, int acierto_test, D
 
 void imprimeResultados(vector<vector<double>> resultados){
     std::cout << "\nParticion\t" << "Tasa_clas_train[%]\t" << "Tasa_clas_test[%]\t" << "Tasa_red[%]\t" << "Fitness\t\t" << "Tiempo[ms]";
-    cout << fixed << setprecision(2);
-    vector<double> media(resultados.size(),0);
+    std::cout << fixed << setprecision(2);
+    vector<double> media(resultados[0].size(),0);
     for(int i = 0; i < resultados.size(); i++){
-        cout << endl;
-        cout << i+1 << "\t\t";
+        std::cout << endl;
+        std::cout << i+1 << "\t\t";
         for(int j = 0; j < resultados[0].size(); j++){
             media[j] += resultados[i][j];
-            cout << resultados[i][j] << "\t\t";
+            std::cout << resultados[i][j] << "\t\t";
             if(j < 2)
-                cout << "\t";
+                std::cout << "\t";
         }
     }
     //MEDIA
-    cout << "\nMEDIA\t\t";
-    for(int i = 0; i < resultados.size(); i++){
-        cout << media[i]/5.0 << "\t\t";
+    std::cout << "\nMEDIA\t\t";
+    for(int i = 0; i < resultados[0].size(); i++){
+        std::cout << media[i]/5.0 << "\t\t";
         if(i < 2)
-            cout << "\t";
+            std::cout << "\t";
     }
-    cout << endl;
+    std::cout << endl;
 }
 
 void imprimePesoMedio(vector<double> pesos){
@@ -122,58 +123,68 @@ int main(int argc, char** argv) {
     Dataset train,test;
     vector<double> pesosMedios;
     vector<vector<double>> pesosParticiones;
-    vector<vector<double>> uno, dos;
-
-    /**Parte 0 y 1: 1NN, GREEDY y resultados**/
-    for(int i = 0; i < 5; i++){
-        //Lee los archivos con el nombre pasado por parámetro
-        if(leerFicheros(argv[1], train, test, i)){
-            std::cout << "Ha surgido un error leyendo los datos\n";
-            return EXIT_FAILURE;
-        }
-
-        //Inicializa variables
-        if(i == 0){ //en la primera iteracion
-            pesosMedios.resize(train.numCaracteristicas(),0);
-        }
-        vector<double> pesos(train.numCaracteristicas(),1);  //en todas las iteraciones
-
-        //Normalizamos los datos
-        normalizar(train, test);
-
-        //Ejecutamos 1NN sin pesos ponderados (todos a 1)
-        auto momentoInicio = high_resolution_clock::now();
-        clasificar(train, test, pesos, acierto_train, acierto_test);
-        uno.push_back(calcularRendimiento(i, acierto_train, acierto_test, train, test, pesos, momentoInicio));
-
-        //Ejecutamos 1NN con pesos ponderados(Greedy)
-        momentoInicio = high_resolution_clock::now();
-        Greedy gr;
-        pesos = gr.greedy_relief(train);
-
-        clasificar(train, test, pesos, acierto_train, acierto_test);
-        dos.push_back(calcularRendimiento(i, acierto_train, acierto_test, train, test, pesos, momentoInicio));
-
-        for(int j = 0; j < pesosMedios.size(); j++){
-            pesosMedios[j] += pesos[j];
-        }
-
-        pesosParticiones.push_back(pesos); //Para imprimirlos al final
+    vector<vector<double>> resultados;
+    vector<string> ficheros = {"diabetes", "ozone-320", "spectf-heart"};
+    string algoritmo = "1-NN";
+    if (argc != 1)
+        algoritmo = argv[1];
+    
+    if(algoritmo != "1-NN" && algoritmo != "greedy"){
+        cerr << "Algoritmo " << argv[1] << " desconocido. Elige: 'sin parametro', greedy o bl.\n";
+        return EXIT_FAILURE;
     }
+    //Lee todos los dataset
+    while(!ficheros.empty()){
+        string fichero = ficheros.front(); //Obtiene el primer elemento
+        ficheros.erase(ficheros.begin()); //Lo saca del vector
 
-    //Clasificador 1NN sin pesos (todo a 1)
-    cout << "---------------------------------------Clasificador 1NN sin pesos:---------------------------------------";
-    imprimeResultados(uno);
+        //Inicializar
+        pesosParticiones = {};
+        pesosMedios = {};
+        resultados = {};
+        for(int i = 0; i < 5; i++){
+            //Lee los archivos con el nombre pasado por parámetro
+            if(leerFicheros(fichero, train, test, i)){
+                std::cout << "Ha surgido un error leyendo los datos\n";
+                return EXIT_FAILURE;
+            }
 
-    //Clasificador 1NN mejorado con greedy
-    cout << "\n---------------------------------------Clasificador 1NN con Greedy:--------------------------------------";
-    imprimeResultados(dos);
+            //Inicializa variables
+            if(i == 0) //en la primera iteracion
+                pesosMedios.resize(train.numCaracteristicas(),0);
+            vector<double> pesos(train.numCaracteristicas(),1);  //en todas las iteraciones
 
-    // //Imprime todos los pesos por particion
-    // imprimePesos(pesosParticiones);
+            //Normalizamos los datos
+            normalizar(train, test);
 
-    //Imprime el peso medio
-    imprimePesoMedio(pesosMedios);
+
+            //Ejecutamos Greedy Relief
+            if(algoritmo == "greedy"){
+                Greedy gr;
+                pesos = gr.greedy_relief(train);
+            }
+
+            //Ejecutamos 1NN
+            auto momentoInicio = chrono::high_resolution_clock::now();
+            clasificar(train, test, pesos, acierto_train, acierto_test);
+            resultados.push_back(calcularRendimiento(i, acierto_train, acierto_test, train, test, pesos, momentoInicio));
+
+            for(int j = 0; j < pesosMedios.size(); j++){
+                pesosMedios[j] += pesos[j];
+            }
+
+            pesosParticiones.push_back(pesos); //Para imprimirlos al final
+        }
+        //Clasificador 1NN sin pesos (todo a 1)
+        std::cout << "--------------------------------------Clasificador (" << algoritmo << "):" << fichero << "-------------------------------------";
+        imprimeResultados(resultados);
+
+        // //Imprime todos los pesos por particion
+        // imprimePesos(pesosParticiones);
+
+        //Imprime el peso medio
+        imprimePesoMedio(pesosMedios);
+    }
 
     return 0;
 }
